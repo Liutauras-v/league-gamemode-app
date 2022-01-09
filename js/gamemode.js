9e1219ffc2240ad7bc30dc9833791bb5efe49abe
Liutauras Vijeikis <liutauras.vijeikis@gmail.com>
2022-01-09 18:32:04 +0200

    Added gems and settings

--- js/gamemode.js
+++ js/gamemode.js
@@ -1,12 +1,31 @@
 export default class Gamemode {
-    
-    
-    constructor(name, desc, cost, role, img){
+
+    constructor(name, desc, cost, role, img) {
         this.name = name;
         this.desc = desc;
         this.cost = cost;
         this.role = role;
         this.img = img;
+    }
+
+    getRarity() {
+
+        const rarities = [
+            { name: "epic", cost_range: 0, color: "#13DFF8", img: "./img/gems/Epic.png", weight: 0 },
+            { name: "legendary", cost_range: 25, color: "#E8101B", img: "./img/gems/legendary.png", weight: 1 },
+            { name: "hextech", cost_range: 50, color: "#D50FCC", img: "./img/gems/Hextech.png", weight: 2 },
+            { name: "ultimate", cost_range: 75, color: "#F8941A", img: "./img/gems/Ultimate.png", weight: 3 }
+        ];
+
+        let max_rarity;
+        rarities.forEach(r => {
+            if (r.cost_range < this.cost) {
+                max_rarity = r;
+            }
+        });
+
+        return max_rarity;
+
     }
 
 }
--- js/main.js
+++ js/main.js
@@ -17,66 +17,68 @@ gamemodes.push(new Gamemode("Magic is overrated", "Ability power items are banne
 gamemodes.push(new Gamemode("Physical", "Attack damage items only", 10, r_builds, "bfsword.png"));
 gamemodes.push(new Gamemode("Weakness", "Attack damage items are banned", 10, r_builds, "nobfsword.png"));
 gamemodes.push(new Gamemode("Rock solid", "Tank / Fighter items only", 10, r_builds, "armor.png"));
-gamemodes.push(new Gamemode("Pillow fight", "Support items only", 10, r_builds, "spellthiefs.png"));
+gamemodes.push(new Gamemode("Pillow fight", "Support items only", 75, r_builds, "spellthiefs.png"));
 gamemodes.push(new Gamemode("What is this healing?", "Grievous wounds items are banned (including ignite)", 10, r_builds, "blood.png"));
 gamemodes.push(new Gamemode("No penetration", "Armor / Magic penetration items are banned (including some item passives that have penetration)", 10, r_builds, "nopen.png"));
-gamemodes.push(new Gamemode("Classic builds", "Mythic items are banned", 10, r_builds, "nomythic.png"));
+gamemodes.push(new Gamemode("Classic builds", "Mythic items are banned", 35, r_builds, "nomythic.png"));
 gamemodes.push(new Gamemode("Mythic power spike", "Mythic item can only be purchased as a last item", 10, r_builds, "tryforce.png"));
 
 gamemodes.push(new Gamemode("Mages", "Mage champions only", 10, r_champPool, "mage.png"));
 gamemodes.push(new Gamemode("Tanks", "Tank champions only", 10, r_champPool, "tank.png"));
 gamemodes.push(new Gamemode("Fighters", "Fighter champions only", 10, r_champPool, "fighter.png"));
 gamemodes.push(new Gamemode("Assassins", "Assassin champions only", 10, r_champPool, "assassin.png"));
-gamemodes.push(new Gamemode("Marksmans", "Marksman champions only", 10, r_champPool, "adc.png"));
-gamemodes.push(new Gamemode("Supports", "Support champions only (Mage supports (I.E. Lux, Swain) are not allowed)", 10, r_champPool, "support.png"));
+gamemodes.push(new Gamemode("Marksmans", "Marksman champions only", 35, r_champPool, "adc.png"));
+gamemodes.push(new Gamemode("Supports", "Support champions only (Mage supports (I.E. Lux, Swain) are not allowed)", 75, r_champPool, "support.png"));
 
 gamemodes.push(new Gamemode("Forgot to buy items", "No items until 5 minutes", 10, r_gameplay, "5min.png"));
-gamemodes.push(new Gamemode("Forgot to buy items... again", "No items until 10 minutes", 10, r_gameplay, "10min.png"));
-gamemodes.push(new Gamemode("I don't need items", "No items until 15 minutes", 10, r_gameplay, "15min.png"));
+gamemodes.push(new Gamemode("Forgot to buy items... again", "No items until 10 minutes", 35, r_gameplay, "10min.png"));
+gamemodes.push(new Gamemode("I don't need items", "No items until 15 minutes", 55, r_gameplay, "15min.png"));
 gamemodes.push(new Gamemode("Bloody items", "No items until first blood", 10, r_gameplay, "firstblood.png"));
 gamemodes.push(new Gamemode("Drake items", "No items until first drake (any team)", 10, r_gameplay, "drakekill.png"));
 gamemodes.push(new Gamemode("Stone items (any team)", "No items until first tower (any team)", 10, r_gameplay, "turret1.png"));
-gamemodes.push(new Gamemode("Stone items", "No items until your team  gets a first tower", 10, r_gameplay, "turret2.png"));
+gamemodes.push(new Gamemode("Stone items", "No items until your team  gets a first tower", 35, r_gameplay, "turret2.png"));
 gamemodes.push(new Gamemode("Bloody items (personal)", "You can only buy items after you get your first kill", 10, r_gameplay, "firstkill.png"));
 gamemodes.push(new Gamemode("ARAM items", "No items until your first death", 10, r_gameplay, "aram.png"));
-gamemodes.push(new Gamemode("Darkness", "Wards are not allowed. Oracle Lens is allowed", 10, r_gameplay, "noward.png"));
+gamemodes.push(new Gamemode("Darkness", "Wards are not allowed. Oracle Lens is allowed", 35, r_gameplay, "noward.png"));
 gamemodes.push(new Gamemode("Bro lane", "Don't fight until 5 minutes", 10, r_gameplay, "cull.png"));
-gamemodes.push(new Gamemode("Nasus on copium", "Don't fight until 10 minutes", 10, r_gameplay, "nasusq.png"));
-gamemodes.push(new Gamemode("*Slaps keyboard*", "Missed cannon forces all summoner spells (until 10 minutes)", 10, r_gameplay, "flash.png"));
-gamemodes.push(new Gamemode("AFK", "Missed cannon forces you to recall (until 10 minutes)", 10, r_gameplay, "recall.png"));
+gamemodes.push(new Gamemode("Nasus on copium", "Don't fight until 10 minutes", 55, r_gameplay, "nasusq.png"));
+gamemodes.push(new Gamemode("*Slaps keyboard*", "Missed cannon forces all summoner spells (until 10 minutes)", 35, r_gameplay, "flash.png"));
+gamemodes.push(new Gamemode("AFK", "Missed cannon forces you to recall (until 10 minutes)", 35, r_gameplay, "recall.png"));
 gamemodes.push(new Gamemode("Jungle curse", "Don't go back while a jungle buff is active (exepct dragon)", 10, r_gameplay, "baron.png"));
-gamemodes.push(new Gamemode("Low elo", "Epic monsters can't be killed", 10, r_gameplay, "nodrake.png"));
+gamemodes.push(new Gamemode("Low elo", "Epic monsters can't be killed", 35, r_gameplay, "nodrake.png"));
 
 gamemodes.push(new Gamemode("Precision rune tree only", "You can only use precision rune tree", 10, r_pregame, "precrune.png"));
 gamemodes.push(new Gamemode("Domination rune tree only", "You can only use domination rune tree", 10, r_pregame, "dominationrune.png"));
 gamemodes.push(new Gamemode("Sorcery rune tree only", "You can only use sorcery rune tree", 10, r_pregame, "sorceryrune.png"));
 gamemodes.push(new Gamemode("Resolve rune tree only", "You can only use resolve rune tree", 10, r_pregame, "resolverune.png"));
 gamemodes.push(new Gamemode("Inspiration rune tree only", "You can only use inspiration rune tree", 10, r_pregame, "insprune.png"));
-gamemodes.push(new Gamemode("Flashless", "Flash summoner spell is banned", 10, r_pregame, "noflash.png"));
-gamemodes.push(new Gamemode("Pro play", "Teleport summoner spell is mandatory", 10, r_pregame, "tp.png"));
-gamemodes.push(new Gamemode("Disco nunu", "Ghost and clense summoner spells are mandatory (ghost and smite for junglers)", 10, r_pregame, "nunu.png"));
+gamemodes.push(new Gamemode("Flashless", "Flash summoner spell is banned", 35, r_pregame, "noflash.png"));
+gamemodes.push(new Gamemode("Pro play", "Teleport summoner spell is mandatory", 35, r_pregame, "tp.png"));
+gamemodes.push(new Gamemode("Disco nunu", "Ghost and clense summoner spells are mandatory (ghost and smite for junglers)", 55, r_pregame, "nunu.png"));
 
-gamemodes.push(new Gamemode("I'm the captain here", "Team captain ignores all other rules", 10, r_captain, "gpface.png"));
-gamemodes.push(new Gamemode("Kill command", "Team captain can ask anyone at any point to die to him (once per game)", 10, r_captain, "killcom.png"));
-gamemodes.push(new Gamemode("Instalock", "Team captain has to play Yasuo or Yone (Yasuo and Yone can't be banned)", 10, r_captain, "yasuo.png"));
-gamemodes.push(new Gamemode("Prisoner", "Team captain can only buy items after getting his first kill", 10, r_captain, "prisoner.png"));
+gamemodes.push(new Gamemode("I'm the captain here", "Team captain ignores all other rules", 55, r_captain, "gpface.png"));
+gamemodes.push(new Gamemode("Kill command", "Team captain can ask anyone at any point to die to him (once per game)", 55, r_captain, "killcom.png"));
+gamemodes.push(new Gamemode("Instalock", "Team captain has to play Yasuo or Yone (Yasuo and Yone can't be banned)", 55, r_captain, "yasuo.png"));
+gamemodes.push(new Gamemode("Prisoner", "Team captain can only buy items after getting his first kill", 55, r_captain, "prisoner.png"));
+gamemodes.push(new Gamemode("Shopkeeper", "Team captain chooses what first item everyone has to build (ignores build restrictions)", 55, r_captain, "boris.png"));
+gamemodes.push(new Gamemode("Follow the king", "Everyone has to build the same items as the team captain", 55, r_captain, "poroking.png"));
 
-gamemodes.push(new Gamemode("Opposite day", "Win by losing", 10, r_legendary, "opday.png"));
-gamemodes.push(new Gamemode("Permadeath", "After dying 3 times - you must stay afk for the rest of the game", 10, r_legendary, "permadeath.png"));
-gamemodes.push(new Gamemode("Nemesis Draft", "Every player picks a champion for the opposing team", 10, r_legendary, "taric.png"));
+gamemodes.push(new Gamemode("Opposite day", "Win by losing", 100, r_legendary, "opday.png"));
+gamemodes.push(new Gamemode("Permadeath", "After dying 3 times - you must stay afk for the rest of the game", 100, r_legendary, "permadeath.png"));
+gamemodes.push(new Gamemode("Nemesis Draft", "Every player picks a champion for the opposing team", 100, r_legendary, "taric.png"));
 
 
 function generateAllRules(gamemodesList, teamName) {
 
     let HTML = '';
-    
-    if(document.querySelector("#teamBalanceBox").checked){
+
+    if (document.querySelector("#teamBalanceBox").checked) {
         HTML += `<p class="team-name">${teamName}</p>`;
     }
 
     gamemodesList.forEach(gm => {
         HTML += '<div class="col">';
-        HTML += '<div class="card shadow-sm">';
+        HTML += `<div class="card card-${gm.getRarity().name}">`;
         HTML += `<span class="card-role" style="border: 2px solid ${gm.role.color}; color: ${gm.role.color};">${gm.role.name}</span>`;
         HTML += '<div class="card-imgbox">';
         HTML += `<img class="card-img-top" src="./img/${gm.img}" alt="${gm.img}" />`;
@@ -84,6 +86,7 @@ function generateAllRules(gamemodesList, teamName) {
         HTML += '<div class="card-body">';
         HTML += `<h5 class="card-text">${gm.name}</h3>`;
         HTML += `<p class="card-text">${gm.desc}</p>`;
+        HTML += `<img class="gem" src="${gm.getRarity().img}">`;
         HTML += '</div></div></div>';
 
     });
@@ -92,24 +95,125 @@ function generateAllRules(gamemodesList, teamName) {
 }
 
 function buildTeam() {
+
+    let rules = {
+        build_pool: document.querySelector('#i_build_pool').value,
+        build_chance: document.querySelector('#i_build_chance').value,
+        champion_pool: document.querySelector('#i_champion_pool').value,
+        champion_chance: document.querySelector('#i_champion_chance').value,
+        gameplay_pool: document.querySelector('#i_gameplay_pool').value,
+        gameplay_chance: document.querySelector('#i_gameplay_chance').value,
+        pregame_pool: document.querySelector('#i_pregame_pool').value,
+        pregame_chance: document.querySelector('#i_pregame_chance').value,
+        captain_pool: document.querySelector('#i_captain_pool').value,
+        captain_chance: document.querySelector('#i_captain_chance').value,
+        legendary_pool: document.querySelector('#i_legendary_pool').value,
+        legendary_chance: document.querySelector('#i_legendary_chance').value,
+        has_captain: document.querySelector("#captainBox").checked,
+        ignore_rarities: document.querySelector("#rarityBox").checked
+    };
+
+    // Chance validation
+    if (rules.build_chance > 100 || rules.build_chance < 0) {
+        rules.build_chance = 100;
+        document.querySelector('#i_build_chance').value = 100;
+    }
+
+    if (rules.champion_chance > 100 || rules.champion_chance < 0) {
+        rules.champion_chance = 100;
+        document.querySelector('#i_champion_chance').value = 100;
+    }
+
+    if (rules.gameplay_chance > 100 || rules.gameplay_chance < 0) {
+        rules.gameplay_chance = 100;
+        document.querySelector('#i_gameplay_chance').value = 100;
+    }
+
+    if (rules.pregame_chance > 100 || rules.pregame_chance < 0) {
+        rules.pregame_chance = 100;
+        document.querySelector('#i_pregame_chance').value = 100;
+    }
+
+    if (rules.captain_chance > 100 || rules.captain_chance < 0) {
+        rules.captain_chance = 100;
+        document.querySelector('#i_legendary_chance').value = 100;
+    }
+
+    if (rules.legendary_chance > 100 || rules.legendary_chance < 0) {
+        rules.legendary_chance = 100;
+        document.querySelector('#i_champion_chance').value = 100;
+    }
+
     let fetched_modes = [];
 
     let g_builds = gamemodes.filter((g) => { return g.role == r_builds });
-    let g_champPool = gamemodes.filter((g) => { return g.role == r_champPool });
+    let g_champion = gamemodes.filter((g) => { return g.role == r_champPool });
     let g_gameplay = gamemodes.filter((g) => { return g.role == r_gameplay });
     let g_pregame = gamemodes.filter((g) => { return g.role == r_pregame });
     let g_captain = gamemodes.filter((g) => { return g.role == r_captain });
     let g_legendary = gamemodes.filter((g) => { return g.role == r_legendary });
 
-    fetched_modes.push(getRandomItem(g_builds));
-    fetched_modes.push(getRandomItem(g_champPool));
-    fetched_modes.push(getRandomItem(g_gameplay));
-    fetched_modes.push(getRandomItem(g_pregame));
-    if (document.querySelector("#captainBox").checked) {
-        fetched_modes.push(getRandomItem(g_captain));
+
+    // Builds
+    if (rules.build_pool > g_builds.length) {
+        rules.build_pool = g_builds.length;
     }
-    if (document.querySelector("#legendaryBox").checked && roleForLegendary(4)) {
-        fetched_modes.push(getRandomItem(g_legendary));
+    for (let i = 0; i < rules.build_pool; i++) {
+        if (roleForCard(rules.build_chance)) {
+            fetched_modes.push(getRandomItem(g_builds, fetched_modes));
+        }
+    }
+
+    // Champions
+    if (rules.champion_pool > g_champion.length) {
+        rules.champion_pool = g_champion.length;
+    }
+    for (let i = 0; i < rules.champion_pool; i++) {
+        if (roleForCard(rules.champion_chance)) {
+            fetched_modes.push(getRandomItem(g_champion, fetched_modes));
+        }
+    }
+
+    // Gameplay
+    if (rules.gameplay_pool > g_gameplay.length) {
+        rules.gameplay_pool = g_gameplay.length;
+    }
+    for (let i = 0; i < rules.gameplay_pool; i++) {
+        if (roleForCard(rules.gameplay_chance)) {
+            fetched_modes.push(getRandomItem(g_gameplay, fetched_modes));
+        }
+    }
+
+    // Pre-game
+    if (rules.pregame_pool > g_pregame.length) {
+        rules.pregame_pool = g_pregame.length;
+    }
+    for (let i = 0; i < rules.pregame_pool; i++) {
+        if (roleForCard(rules.pregame_chance)) {
+            fetched_modes.push(getRandomItem(g_pregame, fetched_modes));
+        }
+    }
+
+    // Captain
+    if (rules.has_captain) {
+        if (rules.captain_pool > g_captain.length) {
+            rules.captain_pool = g_captain.length;
+        }
+        for (let i = 0; i < rules.captain_pool; i++) {
+            if (roleForCard(rules.captain_chance)) {
+                fetched_modes.push(getRandomItem(g_captain, fetched_modes));
+            }
+        }
+    }
+
+    // Legendary
+    if (rules.legendary_pool > g_legendary.length) {
+        rules.legendary_pool = g_legendary.length;
+    }
+    for (let i = 0; i < rules.legendary_pool; i++) {
+        if (roleForCard(rules.legendary_chance)) {
+            fetched_modes.push(getRandomItem(g_legendary, fetched_modes));
+        }
     }
 
     return fetched_modes;
@@ -127,16 +231,28 @@ function generateRules() {
 
 }
 
-function getRandomItem(arr) {
-    return arr[Math.floor((Math.random() * arr.length))];
+function getRandomItem(arr, pool) {
+    let rand = arr[Math.floor((Math.random() * arr.length))];
+    if (pool.indexOf(rand) != -1) {
+        return getRandomItem(arr, pool);
+    }
+    else {
+        return rand;
+    }
 }
 
-function roleForLegendary(chance) {
+function roleForCard(chance) {
     let num = Math.floor(Math.random() * (100 - 1 + 1) + 1);
     if (num <= chance) {
         return true;
     }
     return false;
+}
+
+function toggleSettings() {
+    document.querySelectorAll('.adv-stg-hook').forEach(el => {
+        el.classList.toggle("adv-stg");
+    })
 }
 
 try {
@@ -147,6 +263,7 @@ try {
 
 try {
     document.querySelector("#generate").addEventListener("click", () => { generateRules() });
+    document.querySelector('#advanced-settings').addEventListener('click', () => { toggleSettings() });
 } catch (error) {
     // do nothing
 }

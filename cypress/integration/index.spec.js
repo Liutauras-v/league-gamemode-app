0f167b23bec6126181472b6c8b9137d9fc382a2a
Liutauras Vijeikis <liutauras.vijeikis@gmail.com>
2022-02-19 00:23:12 +0200

    Removed cypress

--- cypress/integration/index.spec.js
+++
@@ -1,5 +0,0 @@
-describe.skip('Home page tests', () => {
-    it('Should roll stuff', () => {
-        cy.visit('127.0.0.1:5500/index.html');
-    });
-});
--- cypress/integration/list.spec.js
+++
@@ -1,8 +0,0 @@
-describe('List test', () => {
-    it('List should be displayed', () => {
-        cy.visit('127.0.0.1:5500/list.html');
-        cy.get('.card').first().find('.card-role').should('contain.text', 'Builds');
-        cy.get('.card').first().find('h5.card-text').should('contain.text', 'Magic is power');
-        cy.get('.card').first().find('p.card-text').should('contain.text', 'Ability power items only');
-    });
-});
